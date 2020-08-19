55c434792293eedfb26dc3474da69a4dc8645202
cocony12 <apap8510@naver.com>
2020-08-19 13:40:25 +0900

    [fix] 각 화면마다 데이터 없을 경우 이모티콘 띄우기

--- frontend/src/components/myactivity/MyLikeVideo.vue
+++ frontend/src/components/myactivity/MyLikeVideo.vue
@@ -1,49 +1,67 @@
 <template>
     <div style="margin:50px">
+        <v-container justify-center="justify-center">
 
-        <v-layout row="row" wrap="wrap">
-            
-                <div style="width:100%; margin:0 auto;">
-                    <span style="font-size:26px; font-weight:600; color:#1976d2 !important;">내가 좋아요한 동영상</span>
-                </div>
-                <v-flex
-                v-for="(item,i) in items"
-                :key="`4${i}`"
-                xs12="xs12"
-                sm6="sm6"
-                md4="md4"
-                lg3="lg3"
-                xl2="xl2">
-                <v-card
-                    tile="tile"
-                    flat="flat"
-                    style="margin-left:10px; margin-top:20px;cursor:pointer;">
-                    <v-img
-                        :src="'http://i3a101.p.ssafy.io/images/' + item.thumbnailUrl"
-                        :lazy-src="'http://i3a101.p.ssafy.io/images/' + item.thumbnailUrl"
-                        aspect-ratio="1.7"
-                        @click="move(`/lecture/player/undefined/${item.lectureId}?order=${item.order}&subId=${item.subId}&subHisId=${item.subHisId}`)"></v-img>
+            <div style="width:100%; margin:0 auto;">
+                <span style="font-size:26px; font-weight:600; color:#1976d2 !important;">내가 좋아요한 동영상</span>
+            </div>
+            <div v-if="items.length > 0">
+            <v-layout row="row" wrap="wrap">
+                
+                    <v-flex
+                    v-for="(item,i) in items"
+                    :key="`4${i}`"
+                    xs12="xs12"
+                    sm6="sm6"
+                    md4="md4"
+                    lg3="lg3"
+                    xl2="xl2">
+                    <v-card
+                        tile="tile"
+                        flat="flat"
+                        style="margin-left:10px; margin-top:20px;cursor:pointer;">
+                        <v-img
+                            :src="'http://i3a101.p.ssafy.io/images/' + item.thumbnailUrl"
+                            :lazy-src="'http://i3a101.p.ssafy.io/images/' + item.thumbnailUrl"
+                            aspect-ratio="1.7"
+                            @click="move(`/lecture/player/undefined/${item.lectureId}?order=${item.order}&subId=${item.subId}&subHisId=${item.subHisId}`)"></v-img>
 
-                    <v-list>
-                        <div @click="move(`/lecture/player/undefined/${item.lectureId}?order=${item.order}&subId=${item.subId}&subHisId=${item.subHisId}`)">
-                            <v-list-item-title>
-                                <h3>{{item.title}}</h3>
-                            </v-list-item-title>
-                            <v-list-item-subtitle>
-                                조회수
-                                {{item.viewCount | convertView}}&nbsp;<v-icon size="16" :color="item.userLikeYn ? 'pink' : 'gray'">mdi-heart</v-icon>{{item.likeCount | convertLike}}
-                            </v-list-item-subtitle>
-                        </div>
-                        <v-avatar class="profile" size="20">
-                            <v-img :src="'http://i3a101.p.ssafy.io/images/' + item.profile"></v-img>
-                        </v-avatar>
-                        <span style="margin-left:5px;font-size:12px">{{item.nickname}}</span>
+                        <v-list>
+                            <div @click="move(`/lecture/player/undefined/${item.lectureId}?order=${item.order}&subId=${item.subId}&subHisId=${item.subHisId}`)">
+                                <v-list-item-title>
+                                    <h3>{{item.title}}</h3>
+                                </v-list-item-title>
+                                <v-list-item-subtitle>
+                                    조회수
+                                    {{item.viewCount | convertView}}&nbsp;<v-icon size="16" :color="item.userLikeYn ? 'pink' : 'gray'">mdi-heart</v-icon>{{item.likeCount | convertLike}}
+                                </v-list-item-subtitle>
+                            </div>
+                            <v-avatar class="profile" size="20">
+                                <v-img :src="'http://i3a101.p.ssafy.io/images/' + item.profile"></v-img>
+                            </v-avatar>
+                            <span style="margin-left:5px;font-size:12px">{{item.nickname}}</span>
 
-                    </v-list>
-                </v-card>
+                        </v-list>
+                    </v-card>
 
-            </v-flex>
-        </v-layout>
+                </v-flex>
+            </v-layout>
+            </div>
+            <div v-else>
+                <v-container fluid style="width:100%;">         
+                    <v-row>
+                        <v-col cols="12">
+                            <v-row align="start" justify="center">                                    
+                                <v-icon style="font-size:150px; color:rgba(0, 0, 0, 0.54); margin:30px 0 20px 0"> mdi-emoticon-cry-outline </v-icon>                               
+                            </v-row>
+                            <v-row align="start" justify="center">       
+                                <div style="font-size:20px; margin-bottom:20px;"> 동영상이 존재하지 않습니다 :( </div>    
+                            </v-row>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </div>
+        </v-container>
     </div>
 </template>
 
--- frontend/src/components/myactivity/Reply.vue
+++ frontend/src/components/myactivity/Reply.vue
@@ -1,67 +1,69 @@
 <template>
     <div style="; margin-top:20px;">
         <v-card tile="tile" flat="flat">
-            <v-layout wrap="wrap">
-                <div v-if="items.length > 0"
-                    ref="question"
-                    :style="{'height': questionHeight+'px'}"
-                    style="width:100%; overflow-y:auto; margin-top:0px;">
-                    <v-list v-if="true" link="link">
-                        <div
-                            class="boardContent"
-                            style="width:100%; border-bottom:1px solid #c8c8c8; cursor:pointer;"
-                            v-for="item in items"
-                            :key="item.boardReplyId"
-                            @click="goToDetail(item.boardId, item.boardType, item.lectureId, item.subId)">
-                            <v-list-item style="width:100%;">
-                                <v-list-item-content style="padding-bottom:0px;">
-                                    <div class="v-list-item__title">{{ item.boardTitle }}</div>
-                                    <div class="v-list-item__subtitle">원문제목 : {{ item.boardContent }}</div>
-                                </v-list-item-content>
-                            </v-list-item>
-                            <v-list-item>
-                                <v-list-item-content style="padding-top:0px;">
-                                    <v-list-item-subtitle>
-                                        <v-list-item-avatar size="15" style="margin:0px;">
-                                            <v-img :src="'http://i3a101.p.ssafy.io/images/' + item.profile"></v-img>
-                                        </v-list-item-avatar>
-                                        <span style="margin-left:3px; font-size:12px;">{{ item.userName }}</span>
-                                        &nbsp;&nbsp;
-                                        <span style="font-size:12px;">
-                                            <v-icon small="small">mdi-comment-text</v-icon>
-                                            {{item.replyCount}}</span>&nbsp;
-                                        <span style="font-size:12px;">조회
-                                            {{item.boardCount}}</span>
+            <div v-if="items.length > 0">
+                <v-layout wrap="wrap">
+                    <div
+                        ref="question"
+                        :style="{'height': questionHeight+'px'}"
+                        style="width:100%; overflow-y:auto; margin-top:0px;">
+                        <v-list v-if="true" link="link">
+                            <div
+                                class="boardContent"
+                                style="width:100%; border-bottom:1px solid #c8c8c8; cursor:pointer;"
+                                v-for="item in items"
+                                :key="item.boardReplyId"
+                                @click="goToDetail(item.boardId, item.boardType, item.lectureId, item.subId)">
+                                <v-list-item style="width:100%;">
+                                    <v-list-item-content style="padding-bottom:0px;">
+                                        <div class="v-list-item__title">{{ item.boardTitle }}</div>
+                                        <div class="v-list-item__subtitle">원문제목 : {{ item.boardContent }}</div>
+                                    </v-list-item-content>
+                                </v-list-item>
+                                <v-list-item>
+                                    <v-list-item-content style="padding-top:0px;">
+                                        <v-list-item-subtitle>
+                                            <v-list-item-avatar size="15" style="margin:0px;">
+                                                <v-img :src="'http://i3a101.p.ssafy.io/images/' + item.profile"></v-img>
+                                            </v-list-item-avatar>
+                                            <span style="margin-left:3px; font-size:12px;">{{ item.userName }}</span>
+                                            &nbsp;&nbsp;
+                                            <span style="font-size:12px;">
+                                                <v-icon small="small">mdi-comment-text</v-icon>
+                                                {{item.replyCount}}</span>&nbsp;
+                                            <span style="font-size:12px;">조회
+                                                {{item.boardCount}}</span>
 
-                                    </v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-                        </div>
-                    </v-list>
+                                        </v-list-item-subtitle>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </div>
+                        </v-list>
 
-                    <v-container>
-                        <v-row justify="center">
-                            <v-col cols="8">
-                                <v-pagination v-model="page" class="my-4" :length="pageCnt"></v-pagination>
-                            </v-col>
-                        </v-row>
-                    </v-container>
-                </div>
-                <div v-else>
-                    <v-container fluid style="width:100%;">         
-                        <v-row>
-                            <v-col cols="12">
-                                <v-row align="start" justify="center">                                    
-                                    <v-icon style="font-size:150px; color:rgba(0, 0, 0, 0.54); margin:30px 0 20px 0"> mdi-emoticon-cry-outline </v-icon>                               
-                                </v-row>
-                                <v-row align="start" justify="center">       
-                                    <div style="font-size:20px; margin-bottom:20px;"> 댓글이 존재하지 않습니다 :( </div>    
-                                </v-row>
-                            </v-col>
-                        </v-row>
-                    </v-container>
-                </div>
-            </v-layout>
+                        <v-container>
+                            <v-row justify="center">
+                                <v-col cols="8">
+                                    <v-pagination v-model="page" class="my-4" :length="pageCnt"></v-pagination>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </div>
+                </v-layout>
+            </div>
+            <div v-else>
+                <v-container fluid style="width:100%;">         
+                    <v-row>
+                        <v-col cols="12">
+                            <v-row align="start" justify="center">                                    
+                                <v-icon style="font-size:150px; color:rgba(0, 0, 0, 0.54); margin:30px 0 20px 0"> mdi-emoticon-cry-outline </v-icon>                               
+                            </v-row>
+                            <v-row align="start" justify="center">       
+                                <div style="font-size:20px; margin-bottom:20px;"> 댓글이 존재하지 않습니다 :( </div>    
+                            </v-row>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </div>
         </v-card>
         <v-snackbar v-model="snackbar" timeout="1500" color="primary">
             {{msg}}
